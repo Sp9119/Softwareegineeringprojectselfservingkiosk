b54fb5f9658ee6cc4db5b7d69fef606d053bdacd
AliAKidwai <alikidwaitx@gmail.com>
2025-11-15 23:04:14 -0600

    Create payment.py

---
+++ payment.py
@@ -0,0 +1,16 @@
+#payment calculater
+import json
+import math
+import Administer
+import Menu
+
+class calculater():
+    def __init__(self, price, name, quantity):
+        self.price = price
+        self.name = name
+        self.quantity = quantity
+
+
+
+
+#payment method
