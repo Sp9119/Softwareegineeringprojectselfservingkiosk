a20cd7038f1c1c39a7ce0de79a183866837cf6c2
Sp9119 <shreyashpatel2099@gmail.com>
2025-11-16 17:53:32 -0600

    Update rating.py

--- rating.py
+++ rating.py
@@ -1,1 +1,13 @@
-
+#input rating
+rating = int(input("Enter your rating: "))
+print("Enter your ratings : 1 to 5")
+if rating == 1:
+    print("Apologies for the inconvience.")
+elif rating == 2:
+     print("We will try to do better next time!")
+elif rating == 3:
+     print("Thank you!")
+elif rating == 4:
+     print("We are glad you enjoyed in our cafe!")
+elif rating == 5:
+     print("Awesome, hope you have wonderful day!")
